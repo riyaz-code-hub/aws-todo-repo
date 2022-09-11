67400e846235c208a2ef8be51e6a4b4ccf59645d
riyaz-code-hub <mdriyazalam786@gmail.com>
2022-09-11 01:57:40 -0700

    # First commit

---
+++ aws-todo-app/src/main/java/com/riyaz/aws/app/AwsTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.riyaz.aws.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTodoAppApplication.class, args);
+	}
+
+}
---
+++ aws-todo-app/src/test/java/com/riyaz/aws/app/AwsTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riyaz.aws.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
