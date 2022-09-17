8e1633c53b080b71a308a47363494c4d39447a3f
riyaz-code-hub <mdriyazalam786@gmail.com>
2022-09-17 00:45:42 -0700

    # db added

---
+++ india-tution-web-app/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO tbt_student(student_id,first_name,middle_name,last_name,email_id,mobile_no)
+values(1,'Riyaz','', 'Alam','riyaz@gmail.com',988998);
+
+
+
---
+++ india-tution-web-app/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE tbt_student(
+student_id INT NOT NULL,
+first_name VARCHAR(50) NOT NULL,
+middle_name VARCHAR(50) NULL,
+last_name VARCHAR(50) NOT NULL,
+email_id VARCHAR(50) NOT NULL,
+mobile_no INT  NULL,
+dob  DATE NULL
+);
